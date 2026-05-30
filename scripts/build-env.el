d95ced3c0f102131eabfd808d73da44952f26f5c
Matt Alexander <alexand2tice@gmail.com>
2026-05-30 16:43:31 -0400

    Automated sync-on-exit (Linux Run): 2026-05-30 16:43:31

--- scripts/build-env.el
+++ scripts/build-env.el
@@ -50,7 +50,7 @@
           (let ((target-file (org-id-find-id-file link-path)))
 	    (cond (target-file
 		   (let*
-		       ((rel-path-from-root (file-relative-name target-file (expand-file-name "public/notes") org-roam-directory))
+		       ((rel-path-from-root (file-relative-name target-file (expand-file-name "public/notes" org-roam-directory)))
 			(base-path (file-name-sans-extension rel-path-from-root))
 			(html-path (concat "https://matt-tice.github.io/Digital-Garden/" base-path ".html"))
 			)
