961c5c2bcc4c63fb639ff4bc48ef3d94ecf5a672
Matt Alexander <alexand2tice@gmail.com>
2026-05-30 20:48:08 -0400

    Automated sync-on-exit (Linux Run): 2026-05-30 20:48:08

--- scripts/build-env.el
+++ scripts/build-env.el
@@ -55,7 +55,6 @@
 			(base-path (file-name-sans-extension rel-path-from-root))
 			(html-path (concat "/Digital-Garden/" base-path ".html"))
 			)
-		     (message "path link is: %s" html-path)
 		     (org-element-put-property link :type "file")
 		     (org-element-put-property link :path html-path)
 		     (org-element-put-property link :raw-link (concat "file:" html-path))
