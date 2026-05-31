0feb141cc84c46a684845d79512823149a5f1e85
Matt Alexander <alexand2tice@gmail.com>
2026-05-31 13:57:16 -0400

    Automated sync-on-exit (Linux Run): 2026-05-31 13:57:16

--- scripts/build-env.el
+++ scripts/build-env.el
@@ -37,78 +37,74 @@
 (defun my/org-link-private-to-stub (link)
   "A robust filter to turn private links into stubs."
   (let* ((contents (org-element-contents link)) ; This will always return a list, not a single piece of text, so we need to grab the text
-				(link-text (if (stringp (car contents))
-					       (car contents)
-					     "Locked Note"))
-				(link-replacement (format "%s [\N{LOCK} Note currently private]" link-text)))
-			   ;; Set the link to the private overwrite
-			   (org-element-put-property link :type "customid")
-			   (org-element-put-property link :path "private-note")
-			   (org-element-set-contents link (list link-replacement))
-			   )
-  ;; (when (org-export-derived-backend-p backend 'org) ; In this part of the program, we're creating a tmp org file
-  ;;   (let* ((case-fold-search t)
-  ;;          (private-regex "\\[\\(.*?\\)\\].*/private/\\(.*?\\)\\.\\(org\\|html\\|md\\)"))
-  ;;     (if (string-match private-regex text)
-  ;;         (let ((link-text (match-string 1 text)))
-  ;;           (format "**%s** [?? Note currently private]" link-text))
-  ;;       text)))
+	 (link-text (if (stringp (car contents))
+			(car contents)
+		      "Locked Note"))
+	 (link-replacement (format "%s [\N{LOCK} Note currently private]" link-text)))
+    ;; Set the link to the private overwrite
+    (org-element-put-property link :type "customid")
+    (org-element-put-property link :path "private-note")
+    (org-element-set-contents link (list link-replacement))
+    )
   )
 
-(defun my/org-export-sanitize-documents-path (text backend info)
-  "Scrub the user's home directory from the final GitHub output."
-  (when (org-export-derived-backend-p backend 'org)
-    (let ((user-home (expand-file-name "~")))
-      (replace-regexp-in-string (regexp-quote user-home) "[HOME]/" text))))
+
+;; --------- Consider adding this filter back at some point ---------
+
+;; (defun my/org-export-sanitize-documents-path (text backend info)
+;;   "Scrub the user's home directory from the final GitHub output."
+;;   (when (org-export-derived-backend-p backend 'org)
+;;     (let ((user-home (expand-file-name "~")))
+;;       (replace-regexp-in-string (regexp-quote user-home) "[HOME]/" text))))
 
 ;; Turn org-roam links into regular org-links (which github can understand)
 (defun my/org-export-resolve-org-ids (tree backend info)
   "Traverses the Org AST and mutates raw 'id:' link objects into standard relative 'file:' links."
 
   (when (org-export-derived-backend-p backend 'org)
-  (org-element-map tree 'link
-    (lambda (link)
-      (let ((link-type (org-element-property :type link))
-            (link-path (org-element-property :path link))) ;; This will grab the org-roam id of the link
-        ;; Only target links that use the id protocol
-        (when (string= link-type "id")
-	  
-	  (when (null org-id-locations)	;; Typically the org-id-locations database hasn't loaded by the time we get here, so we load it now if it hasn't been already
-	    (org-id-locations-load))
-	  
-          (let ((target-file (gethash link-path org-id-locations)))
-	    (cond
-	     ( (null target-file)
-	       (my/org-link-private-to-stub link))
+    (org-element-map tree 'link
+      (lambda (link)
+	(let ((link-type (org-element-property :type link))
+              (link-path (org-element-property :path link))) ;; This will grab the org-roam id of the link
+          ;; Only target links that use the id protocol
+          (when (string= link-type "id")
+	    
+	    (when (null org-id-locations)	;; Typically the org-id-locations database hasn't loaded by the time we get here, so we load it now if it hasn't been already
+	      (org-id-locations-load))
+	    
+            (let ((target-file (gethash link-path org-id-locations)))
+	      (cond
+	       ( (null target-file)
+		 (my/org-link-private-to-stub link))
 
-	     (target-file
-		   (let ((normalized-target (expand-file-name target-file)))
-		     (message "Target: %s" normalized-target)
-		     (message "Contents: %s" (org-element-contents link))
-		     (cond
-		      ;; ----- Case 1: Private Notes -----
-		      ((string-match "/private/" normalized-target)
-		       (my/org-link-private-to-stub ))
-		      
-		      ;; ----- Case 2: Public Notes -----
-		     ((string-match "/notes/" normalized-target)
-			   (let* (	;; In this case we preserve the link content, and just change the path to point to the relevant html file
-				  (notes-root (expand-file-name "../../notes" ))
-				  (rel-path-from-notes (file-relative-name normalized-target notes-root))
-				  (base-path (file-name-sans-extension rel-path-from-notes))
-				  (html-path (concat "/Digital-Garden/" base-path ".html"))
-				  )
-			     (org-element-put-property link :type "https")
-			     (org-element-put-property link :path html-path)
-			     ))
-		     )))
-		  (t
-		   (message "[WARNING] Broken org-id link found in file: %s (Target ID: %s)" (buffer-file-name) link-path)
-		   (org-element-put-property link :type "customid")
-		   (org-element-put-property link :path "broken-link"))
-		  )
-	    ))))))
-;; Since you're modifying the AST, you need to return the whole tree at the end
+	       (target-file
+		(let ((normalized-target (expand-file-name target-file)))
+		  (message "Target: %s" normalized-target)
+		  (message "Contents: %s" (org-element-contents link))
+		  (cond
+		   ;; ----- Case 1: Private Notes -----
+		   ((string-match "/private/" normalized-target)
+		    (my/org-link-private-to-stub link))
+		   
+		   ;; ----- Case 2: Public Notes -----
+		   ((string-match "/notes/" normalized-target)
+		    (let* (	;; In this case we preserve the link content, and just change the path to point to the relevant html file
+			   (notes-root (expand-file-name "../../notes" ))
+			   (rel-path-from-notes (file-relative-name normalized-target notes-root))
+			   (base-path (file-name-sans-extension rel-path-from-notes))
+			   (html-path (concat "/Digital-Garden/" base-path ".html"))
+			   )
+		      (org-element-put-property link :type "https")
+		      (org-element-put-property link :path html-path)
+		      ))
+		   )))
+	       (t
+		(message "[WARNING] Broken org-id link found in file: %s (Target ID: %s)" (buffer-file-name) link-path)
+		(org-element-put-property link :type "customid")
+		(org-element-put-property link :path "broken-link"))
+	       )
+	      ))))))
+  ;; Since you're modifying the AST, you need to return the whole tree at the end
   tree)
 
 ;; --- 4. The Unified Cloud Export Driver ---
@@ -126,10 +122,6 @@
     (with-current-buffer (find-file-noselect abs-infile)
       (let ((org-export-filter-parse-tree-functions
 	     '(my/org-export-resolve-org-ids))
-            ;; (org-export-filter-final-output-functions
-	    ;;  '(my/org-export-private-to-stub
-	    ;;    my/org-export-sanitize-documents-path
-	    ;;    ))
             (default-directory dest-dir))
         (org-export-to-file 'org temp-org nil nil nil nil nil))
       (kill-buffer))
