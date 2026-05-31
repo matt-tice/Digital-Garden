74311ad0b86c9ac5d25d515fc96d5561f853e092
Matty <alexand2tice@gmail.com>
2026-05-30 22:50:19 -0400

    Automated sync-on-exit (Windows Run): 2026-05-30 22:50:19.26

--- scripts/build-env.el
+++ scripts/build-env.el
@@ -65,7 +65,7 @@
 	    (cond (target-file
 		   (let*
 		       ((normalized-target (expand-file-name target-file))
-			(repo-root-path "notes") 
+			(repo-root-path (expand-file-name "notes") )
 			(rel-path-from-root (file-relative-name normalized-target repo-root-path))
 			(base-path (file-name-sans-extension rel-path-from-root))
 			(html-path (concat "/Digital-Garden/" base-path ".html"))
